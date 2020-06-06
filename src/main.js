45eb7460936d17510f6eb80df9b72c543df5ed0f
Merah Soheyb <soheybemir@hotmail.fr>
2020-06-06 11:44:49 +0100

    implement sendNotifications

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 const express = require('express')
 var cors = require('cors')
-var bodyParser  = require('body-parser')
+var bodyParser = require('body-parser')
 const app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json());
 app.use(cors())
 
 /**
@@ -15,35 +16,50 @@ app.use(cors())
  * @return search results matching criteria || bad input parameter
  */
 app.get('/notifications', function (req, res) {
-  let { user, read } = req.query
+    let {user, read} = req.query
 
-  if (user===undefined || user==='') {
-    res.send({error: 'bad input parameter'})
-    return;
-  }
+    if (user === undefined || user === '') {
+        res.send({error: 'bad input parameter'})
+        return;
+    }
 
-  if (read===undefined) read = false
+    if (read === undefined) read = false
 
-  //TODO get notification by user from DB
+    //TODO get notification by user from DB
 
-  // if OK
-  const notification = {
-    "uuid": "d290f1ee-6c54-4b01-90e6-d701748f0851",
-    "id": 1,
-    "sender": 1,
-    "receiver": 2,
-    "creationDate": "2020-08-29T09:12:33.001Z",
-    "read": false,
-    "updateDate": "2020-08-29T09:13:32.021Z",
-    "retired": false
-  }
-  res.send(notification)
+    // if OK
+    const notification = {
+        "uuid": "d290f1ee-6c54-4b01-90e6-d701748f0851",
+        "id": 1,
+        "sender": 1,
+        "receiver": 2,
+        "creationDate": "2020-08-29T09:12:33.001Z",
+        "read": false,
+        "updateDate": "2020-08-29T09:13:32.021Z",
+        "retired": false
+    }
+    res.send(notification)
 })
 
 app.post('/notifications', function (req, res) {
+    let {content, receivers} = req.body
+    console.log(req.body)
+    if (content === undefined || receivers === undefined) {
+        res.send({error: 'object invalid'})
+        return;
+    }
 
+    if (content === '' || receivers.length === 0) {
+        res.send({error: 'bad input parameter'})
+        return;
+    }
+
+    //TODO add {content} in DB to {receivers}
+
+    // if OK
+    res.send({message: 'notifications sent'})
 })
 
 app.listen(4000, function () {
-  console.log('App listening on port 4000!')
+    console.log('App listening on port 4000!')
 })
