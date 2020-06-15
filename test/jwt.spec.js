75a291636810fcb43539a2cd3fd3f349244e88f7
Merah Soheyb <soheybemir@hotmail.fr>
2020-06-15 13:46:35 +0100

    test work around

--- test/jwt.spec.js
+++ test/jwt.spec.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 const {verifyToken} = require('./../src/utils/jwt_auth')
 
 require('dotenv').config()
-const SECRET_KEY = process.env.JWT_SECRET;
+const SECRET_KEY = process.env.JWT_SECRET || "wow";
 
 describe("Test JWT Authorization", function(){
     const token = jwt.sign({uuid: "d290f1ee-6c54-4b01-90e6-d701748f0851"}, SECRET_KEY);
